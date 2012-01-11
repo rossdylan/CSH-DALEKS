eb3e72bd69fcc2d66941853c1e91ae8610ad3d03
Will Dignazio <slackwill@csh.rit.edu>
2012-01-11 14:22:48 -0500

    Started shell app
    Added rshell, a simple shell app for controlling the roomba.

---
+++ rshell.py
@@ -0,0 +1,14 @@
+""" 
+	Program: Roomba Interface Shell
+	Date: 01/11/2012
+	Author: Will Dignazio
+	Description: 
+		Simple shell program for running commands to 
+		a roomba attached to the pc. Using the roomba functions
+		provided in hxRoomba, this shell will allow a user to 
+		create a controller object. The controller object, along
+		with the methods provided, will allow users full control of 
+		the roomba.
+"""
+
+from hxRoomba import *
