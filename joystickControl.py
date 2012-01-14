bed4dfc798dee7fd923fe1ebb0a1902b9c6ce7af
posiden <posiden@helixoide.com>
2012-01-13 21:50:34 -0500

    added reenage on cliff functionality

--- joystickControl.py
+++ joystickControl.py
@@ -11,70 +11,88 @@ if __name__ == "__main__":
 	movingForward = False
 	movingBackward = False
 	zeroCount = 0
-	last3 = []
 	#axis 1: -1=forward, 1=backwards
 	#axis 0: -1=strafe left, 1=strafe right
 	#axis 2: -1=turn left, 1=turn right
+	lastSpeedChange = 0
 	try:
 		while True:
 			pygame.event.pump()
 			for i in range(0, j.get_numaxes()):
 				axisData = j.get_axis(i)
 				if i == 1:
-					last3.append(axisData)
-					if len(last3) > 3:
-						last3 = last3[1:]
-						diff = math.fabs(last3[2] - last3[0])
-						if diff > .3 and diff <= 0.5:
-							roomba.setSpeed(2)
-						elif diff > 0.5 and diff <= 1.0:
-							roomba.setSpeed(3)
 					if axisData == 0.00:
 						zeroCount += 1
 					else:
 						zeroCount = 0
 				if axisData > 0.00 or axisData < 0.00:
+					if roomba.onCliff():
+						roomba.engage()
 					if i == 1:
 						if axisData < 0.00:
 							if movingForward == False:
-								roomba.forward()
+								try:
+									roomba.forward()
+								except Exception:
+									continue
 								movingForward = True
 
 						if axisData > 0.00:
 							if movingBackward == False:
-								roomba.backward()
+								try:
+									roomba.backward()
+								except Exception:
+									continue
 								movingBackward = True
 
 					if i == 2:
 						if axisData > 0.00:
 							if turning == False:
-								roomba.right()
+								try:
+									roomba.right()
+								except Exception:
+									continue
 								turning = True
 
 						if axisData < 0.00:
 							if turning == False:
-								roomba.left()
+								try:
+									roomba.left()
+								except Exception:
+									continue
 								turning = True
+					if i == 3:
+						if math.fabs(lastSpeedChange - axisData) >= 0.5:
+							try:
+								if axisData == 0.00:
+									roomba.setSpeed(2)
+								if axisData >= 0.90:
+									roomba.setSpeed(3)
+								if axisData <= -0.90:
+									roomba.setSpeed(1)
+								lastSpeedChange = axisData
+							except Exception:
+								continue
 				elif axisData == 0.00 and (i == 0 or i == 2):
-					if turning == True and i == 2:
-						print "Sending turning stop"
-						roomba.stop()
-						turning = False
-						roomba.setSpeed(1)
-					if movingForward == True and i == 0:
-						if zeroCount > 3:
-							print "sending forward stop"
+					try:
+						if turning == True and i == 2:
+							print "Sending turning stop"
 							roomba.stop()
-							movingForward = False
-							roomba.setSpeed(1)
-					if movingBackward == True and i == 0:
-						invalid = False
-						if zeroCount > 3:
-							print "sending backward stop"
-							roomba.stop()
-							movingBackward = False
-							roomba.setSpeed(1)
-
+							turning = False
+						if movingForward == True and i == 0:
+							if zeroCount > 3:
+								print "sending forward stop"
+								roomba.stop()
+								movingForward = False
+						if movingBackward == True and i == 0:
+							invalid = False
+							if zeroCount > 3:
+								print "sending backward stop"
+								roomba.stop()
+								movingBackward = False
+					except Exception:
+						continue
 
 	except KeyboardInterrupt:
 		j.quit()
+
